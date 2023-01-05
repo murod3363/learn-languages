b4f83ab72e0eca1fdb329c4371e697bd0ece9951
Murod <murod@com>
2023-01-04 18:27:22 -1000

    ended

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+let menu = document.querySelector(".menu");
 let cansel = document.querySelector(".cansel");
-console.log(cansel);
+let nav =  document.querySelector(".haeder__top-inner")
+console.log(menu);
+menu.addEventListener("click", () => {
+    nav.style.transform = "translateX(0)" 
+} )
+cansel.addEventListener("click", () => {
+    nav.style.transform = "translateX(-101%)" 
+} )
